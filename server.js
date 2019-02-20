4652c0d6e09cd8b6c4a61049e0e444bcf6612d76
Marvin Wichmann <marvin.wichmann@unic.com>
2019-02-20 21:39:27 +0100

    First docker version - use cache

--- server.js
+++ server.js
@@ -8,4 +8,6 @@ server.use(prerender.sendPrerenderHeader());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
 
+server.use(require('prerender-memory-cache'))
+
 server.start();
